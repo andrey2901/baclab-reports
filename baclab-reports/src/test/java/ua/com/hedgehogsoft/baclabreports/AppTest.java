03dfd98ed5acc4b6fb249cd8d95de75394081f5e
Andrii Vynnyk <andrey2901@ua.fm>
2016-01-07 13:28:38 +0200

    Fix the headless exception during tests.

--- baclab-reports/src/test/java/ua/com/hedgehogsoft/baclabreports/AppTest.java
+++ baclab-reports/src/test/java/ua/com/hedgehogsoft/baclabreports/AppTest.java
@@ -2,21 +2,25 @@ package ua.com.hedgehogsoft.baclabreports;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import ua.com.hedgehogsoft.baclabreports.localization.MessageByLocaleService;
 
 public class AppTest extends DefaultTest
 {
    private RestTemplate restTemplate = new TestRestTemplate();
    private String url = "http://localhost:%s/greeting";
-   @Value("${server.port}") private String port;
+   private @Value("${server.port}") String port;
+   private @Autowired MessageByLocaleService messageByLocaleService;
 
    @Test
    public void greeting()
    {
       String formattedURL = String.format(url, port);
       String greetingResponse = restTemplate.getForObject(formattedURL, String.class);
-      assertEquals("Greetings from Spring Boot!", greetingResponse);
+      assertEquals(messageByLocaleService.getMessage("greeting"), greetingResponse);
    }
 }
