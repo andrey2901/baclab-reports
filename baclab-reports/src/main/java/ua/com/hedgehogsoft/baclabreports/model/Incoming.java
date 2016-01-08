9855991c8762ee1bf2dcabb51e71dbcb6bccd885
Andrii Vynnyk <andrey2901@ua.fm>
2016-01-08 14:13:34 +0200

    Adds missed JPA annotation to model entities.

--- baclab-reports/src/main/java/ua/com/hedgehogsoft/baclabreports/model/Incoming.java
+++ baclab-reports/src/main/java/ua/com/hedgehogsoft/baclabreports/model/Incoming.java
@@ -5,12 +5,16 @@ import java.math.RoundingMode;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "INCOMINGS")
 public class Incoming implements ModelEntity<Long>
 {
    @Id
