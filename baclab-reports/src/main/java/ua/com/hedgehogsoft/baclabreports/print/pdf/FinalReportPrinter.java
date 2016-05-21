63178e00a2b78b2f9255a40afabc6db43a915ebb
Andrii Vynnyk <andrey2901@ua.fm>
2016-05-22 02:16:19 +0300

    Re-factoring

--- baclab-reports/src/main/java/ua/com/hedgehogsoft/baclabreports/print/pdf/FinalReportPrinter.java
+++ baclab-reports/src/main/java/ua/com/hedgehogsoft/baclabreports/print/pdf/FinalReportPrinter.java
@@ -1,8 +1,5 @@
 package ua.com.hedgehogsoft.baclabreports.print.pdf;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,248 +12,230 @@ import org.springframework.stereotype.Component;
 
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
 
 @Component
-public class FinalReportPrinter
+public class FinalReportPrinter extends ReportPrinter
 {
    private static final Logger logger = Logger.getLogger(FinalReportPrinter.class);
 
-   public File print(JTable table, String dateFrom, String dateTo)
+   @Override
+   protected Logger getLogger()
    {
-      File file = null;
-      try
+      return logger;
+   }
+
+   @Override
+   protected String resolveFileName(String... args)
+   {
+      String dateFrom = args[0];
+      String dateTo = args[1];
+      return "Загальний_звіт_" + dateFrom + "_" + dateTo + ".pdf";
+   }
+
+   @Override
+   protected void setContent(Document document, JTable table, String... args) throws DocumentException
+   {
+      String dateFrom = args[0];
+      String dateTo = args[1];
+
+      Paragraph paragraph = new Paragraph();
+      paragraph.setFont(font);
+      paragraph.setSpacingAfter(1);
+      paragraph.setSpacingBefore(1);
+      paragraph.setAlignment(Element.ALIGN_CENTER);
+      paragraph.setIndentationLeft(1);
+      paragraph.setIndentationRight(1);
+
+      Paragraph paragraph1 = new Paragraph();
+      paragraph1.setFont(font);
+      paragraph1.setSpacingAfter(3);
+      paragraph1.setSpacingBefore(3);
+      paragraph1.setAlignment(Element.ALIGN_CENTER);
+      Chunk chunk1 = new Chunk("Звіт");
+      paragraph1.add(chunk1);
+
+      Paragraph paragraph2 = new Paragraph();
+      paragraph2.setFont(font);
+      paragraph2.setSpacingAfter(3);
+      paragraph2.setSpacingBefore(3);
+      paragraph2.setAlignment(Element.ALIGN_CENTER);
+      Chunk chunk2 = new Chunk("про надходження і відпуск (використання) лікарських засобів та медичних виробів");
+      paragraph2.add(chunk2);
+
+      Paragraph paragraph3 = new Paragraph();
+      paragraph3.setFont(font);
+      paragraph3.setSpacingAfter(3);
+      paragraph3.setSpacingBefore(3);
+      paragraph3.setAlignment(Element.ALIGN_CENTER);
+      Chunk chunk4 = new Chunk("з " + dateFrom + " до " + dateTo);
+      paragraph3.add(chunk4);
+
+      paragraph.add(paragraph1);
+      paragraph.add(paragraph2);
+      paragraph.add(paragraph3);
+
+      document.add(paragraph);
+
+      /*******************************************************************************************/
+
+      font.setSize(10);
+
+      TableModel model = table.getModel();
+
+      PdfPTable pdfTable = new PdfPTable(model.getColumnCount() - 1);
+
+      pdfTable.setWidths(new int[] {2, 21, 3, 4, 4, 4, 4});
+
+      pdfTable.setWidthPercentage(90);
+
+      PdfPCell cell = null;
+      cell = new PdfPCell(new Phrase("№ з/п", font));
+      cell.setRowspan(2);
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Найменування лікарських засобів та медичних виробів", font));
+      cell.setRowspan(2);
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Одини-\nця\nвиміру", font));
+      cell.setRowspan(2);
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Кількість", font));
+      cell.setColspan(4);
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Залишок на початок періоду", font));
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Надход-\nження", font));
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Викорис-\nтання", font));
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      pdfTable.addCell(cell);
+
+      cell = new PdfPCell(new Phrase("Залишок на\nкінець\nперіоду", font));
+      cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+      pdfTable.addCell(cell);
+
+      Map<String, List<PdfPCell>> groupedCells = new HashMap<>();
+
+      for (int row = 0; row < model.getRowCount(); row++)
       {
-         BaseFont bf = null;
-         try
+         String group = (String) model.getValueAt(row, 7);
+         if (!groupedCells.containsKey(group))
          {
-            bf = BaseFont.createFont(System.getProperty("local.fonts"), BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            cell = new PdfPCell(new Phrase(group, font));
+            cell.setColspan(7);
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            groupedCells.put(group, new ArrayList<>());
+            groupedCells.get(group).add(cell);
          }
-         catch (IOException e)
+         List<PdfPCell> cells = groupedCells.get(group);
+         for (int column = 0; column < model.getColumnCount(); column++)
          {
-            logger.error("The font wasn't loaded.", e);
-         }
-         Font font = new Font(bf);
-
-         font.setSize(14);
-
-         Document document = new Document(PageSize.A4, 0, 0, 0, 0);
-
-         file = new File(System.getProperty("report.folder") + "/Загальний_звіт_" + dateFrom + "_" + dateTo + ".pdf");
-         PdfWriter.getInstance(document, new FileOutputStream(file));
-
-         document.open();
-
-         Paragraph paragraph = new Paragraph();
-         paragraph.setFont(font);
-         paragraph.setSpacingAfter(1);
-         paragraph.setSpacingBefore(1);
-         paragraph.setAlignment(Element.ALIGN_CENTER);
-         paragraph.setIndentationLeft(1);
-         paragraph.setIndentationRight(1);
-
-         Paragraph paragraph1 = new Paragraph();
-         paragraph1.setFont(font);
-         paragraph1.setSpacingAfter(3);
-         paragraph1.setSpacingBefore(3);
-         paragraph1.setAlignment(Element.ALIGN_CENTER);
-         Chunk chunk1 = new Chunk("Звіт");
-         paragraph1.add(chunk1);
-
-         Paragraph paragraph2 = new Paragraph();
-         paragraph2.setFont(font);
-         paragraph2.setSpacingAfter(3);
-         paragraph2.setSpacingBefore(3);
-         paragraph2.setAlignment(Element.ALIGN_CENTER);
-         Chunk chunk2 = new Chunk("про надходження і відпуск (використання) лікарських засобів та медичних виробів");
-         paragraph2.add(chunk2);
-
-         Paragraph paragraph3 = new Paragraph();
-         paragraph3.setFont(font);
-         paragraph3.setSpacingAfter(3);
-         paragraph3.setSpacingBefore(3);
-         paragraph3.setAlignment(Element.ALIGN_CENTER);
-         Chunk chunk4 = new Chunk("з " + dateFrom + " до " + dateTo);
-         paragraph3.add(chunk4);
-
-         paragraph.add(paragraph1);
-         paragraph.add(paragraph2);
-         paragraph.add(paragraph3);
-
-         document.add(paragraph);
-
-         /*******************************************************************************************/
-
-         font.setSize(10);
-
-         TableModel model = table.getModel();
-
-         PdfPTable pdfTable = new PdfPTable(model.getColumnCount() - 1);
-
-         pdfTable.setWidths(new int[] {2, 21, 3, 4, 4, 4, 4});
-
-         pdfTable.setWidthPercentage(90);
-
-         PdfPCell cell = null;
-         cell = new PdfPCell(new Phrase("№ з/п", font));
-         cell.setRowspan(2);
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Найменування лікарських засобів та медичних виробів", font));
-         cell.setRowspan(2);
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Одини-\nця\nвиміру", font));
-         cell.setRowspan(2);
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Кількість", font));
-         cell.setColspan(4);
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Залишок на початок періоду", font));
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Надход-\nження", font));
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Викорис-\nтання", font));
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         pdfTable.addCell(cell);
-
-         cell = new PdfPCell(new Phrase("Залишок на\nкінець\nперіоду", font));
-         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-         pdfTable.addCell(cell);
-
-         Map<String, List<PdfPCell>> groupedCells = new HashMap<>();
-
-         for (int row = 0; row < model.getRowCount(); row++)
-         {
-            String group = (String) model.getValueAt(row, 7);
-            if (!groupedCells.containsKey(group))
+            switch (column)
             {
-               cell = new PdfPCell(new Phrase(group, font));
-               cell.setColspan(7);
-               cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-               groupedCells.put(group, new ArrayList<>());
-               groupedCells.get(group).add(cell);
-            }
-            List<PdfPCell> cells = groupedCells.get(group);
-            for (int column = 0; column < model.getColumnCount(); column++)
-            {
-               switch (column)
-               {
-                  case 0:
-                     cell = new PdfPCell(new Phrase(Integer.toString((int) model.getValueAt(row, column)), font));
-                     cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                     cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                     cells.add(cell);
-                     break;
-                  case 1:
-                  case 2:
-                     cell = new PdfPCell(new Phrase((String) model.getValueAt(row, column), font));
-                     cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                     cells.add(cell);
-                     break;
-                  case 3:
-                  case 4:
-                  case 5:
-                  case 6:
-                     cell = new PdfPCell(
-                           new Phrase((String) Double.toString((double) model.getValueAt(row, column)), font));
-                     cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                     cells.add(cell);
-                     break;
-               }
+               case 0:
+                  cell = new PdfPCell(new Phrase(Integer.toString((int) model.getValueAt(row, column)), font));
+                  cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                  cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                  cells.add(cell);
+                  break;
+               case 1:
+               case 2:
+                  cell = new PdfPCell(new Phrase((String) model.getValueAt(row, column), font));
+                  cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                  cells.add(cell);
+                  break;
+               case 3:
+               case 4:
+               case 5:
+               case 6:
+                  cell = new PdfPCell(
+                        new Phrase((String) Double.toString((double) model.getValueAt(row, column)), font));
+                  cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                  cells.add(cell);
+                  break;
             }
          }
-         for (String group : groupedCells.keySet())
-         {
-            for (PdfPCell printCell : groupedCells.get(group))
-            {
-               pdfTable.addCell(printCell);
-            }
-         }
-         document.add(pdfTable);
-
-         /*******************************************************************************************/
-
-         document.add(Chunk.NEWLINE);
-
-         Paragraph subscribeParagraph = new Paragraph();
-         subscribeParagraph.setFont(font);
-
-         Paragraph laboratoryParagraph = new Paragraph();
-         laboratoryParagraph.setFont(font);
-         laboratoryParagraph.setSpacingAfter(3);
-         laboratoryParagraph.setSpacingBefore(3);
-         laboratoryParagraph.setAlignment(Element.ALIGN_RIGHT);
-         Chunk labVacancyChunk = new Chunk("Лаборант з бактеріології      ");
-         Chunk labUnderlineChunk = new Chunk("                                       ");
-         labUnderlineChunk.setUnderline(0.1f, -0.5f);
-         Chunk labNameChunk = new Chunk("       Н.В. Нагорна            ");
-         laboratoryParagraph.add(labVacancyChunk);
-         laboratoryParagraph.add(labUnderlineChunk);
-         laboratoryParagraph.add(labNameChunk);
-
-         Paragraph accountParagraph = new Paragraph();
-         accountParagraph.setFont(font);
-         accountParagraph.setSpacingAfter(3);
-         accountParagraph.setSpacingBefore(3);
-         accountParagraph.setAlignment(Element.ALIGN_RIGHT);
-         Chunk accVacancyChunk = new Chunk("Бухгалтер      ");
-         Chunk accUnderlineChunk = new Chunk("                                       ");
-         accUnderlineChunk.setUnderline(0.1f, -0.5f);
-         Chunk accNameChunk = new Chunk("      І.Ф. Кольцова            ");
-         accountParagraph.add(accVacancyChunk);
-         accountParagraph.add(accUnderlineChunk);
-         accountParagraph.add(accNameChunk);
-
-         Paragraph deputyParagraph = new Paragraph();
-         deputyParagraph.setFont(font);
-         deputyParagraph.setSpacingAfter(3);
-         deputyParagraph.setSpacingBefore(3);
-         deputyParagraph.setAlignment(Element.ALIGN_RIGHT);
-         Chunk deputyVacancyChunk = new Chunk("Затверджую зам.головного лікаря з медичної частини      ");
-         Chunk deputyUnderlineChunk = new Chunk("                                       ");
-         deputyUnderlineChunk.setUnderline(0.1f, -0.5f);
-         Chunk deputyNameChunk = new Chunk("    І.А. Єсауленко            ");
-         deputyParagraph.add(deputyVacancyChunk);
-         deputyParagraph.add(deputyUnderlineChunk);
-         deputyParagraph.add(deputyNameChunk);
-
-         subscribeParagraph.add(laboratoryParagraph);
-         subscribeParagraph.add(accountParagraph);
-         subscribeParagraph.add(deputyParagraph);
-
-         document.add(subscribeParagraph);
-
-         /*******************************************************************************************/
-
-         document.close();
       }
-      catch (Exception e)
+      for (String group : groupedCells.keySet())
       {
-         logger.error("Загальний_звіт_" + dateFrom + "_" + dateTo + ".pdf wasn't printed.", e);
+         for (PdfPCell printCell : groupedCells.get(group))
+         {
+            pdfTable.addCell(printCell);
+         }
       }
-      return file;
+      document.add(pdfTable);
+
+      /*******************************************************************************************/
+
+      document.add(Chunk.NEWLINE);
+
+      Paragraph subscribeParagraph = new Paragraph();
+      subscribeParagraph.setFont(font);
+
+      Paragraph laboratoryParagraph = new Paragraph();
+      laboratoryParagraph.setFont(font);
+      laboratoryParagraph.setSpacingAfter(3);
+      laboratoryParagraph.setSpacingBefore(3);
+      laboratoryParagraph.setAlignment(Element.ALIGN_RIGHT);
+      Chunk labVacancyChunk = new Chunk("Лаборант з бактеріології      ");
+      Chunk labUnderlineChunk = new Chunk("                                       ");
+      labUnderlineChunk.setUnderline(0.1f, -0.5f);
+      Chunk labNameChunk = new Chunk("       Н.В. Нагорна            ");
+      laboratoryParagraph.add(labVacancyChunk);
+      laboratoryParagraph.add(labUnderlineChunk);
+      laboratoryParagraph.add(labNameChunk);
+
+      Paragraph accountParagraph = new Paragraph();
+      accountParagraph.setFont(font);
+      accountParagraph.setSpacingAfter(3);
+      accountParagraph.setSpacingBefore(3);
+      accountParagraph.setAlignment(Element.ALIGN_RIGHT);
+      Chunk accVacancyChunk = new Chunk("Бухгалтер      ");
+      Chunk accUnderlineChunk = new Chunk("                                       ");
+      accUnderlineChunk.setUnderline(0.1f, -0.5f);
+      Chunk accNameChunk = new Chunk("      І.Ф. Кольцова            ");
+      accountParagraph.add(accVacancyChunk);
+      accountParagraph.add(accUnderlineChunk);
+      accountParagraph.add(accNameChunk);
+
+      Paragraph deputyParagraph = new Paragraph();
+      deputyParagraph.setFont(font);
+      deputyParagraph.setSpacingAfter(3);
+      deputyParagraph.setSpacingBefore(3);
+      deputyParagraph.setAlignment(Element.ALIGN_RIGHT);
+      Chunk deputyVacancyChunk = new Chunk("Затверджую зам.головного лікаря з медичної частини      ");
+      Chunk deputyUnderlineChunk = new Chunk("                                       ");
+      deputyUnderlineChunk.setUnderline(0.1f, -0.5f);
+      Chunk deputyNameChunk = new Chunk("    І.А. Єсауленко            ");
+      deputyParagraph.add(deputyVacancyChunk);
+      deputyParagraph.add(deputyUnderlineChunk);
+      deputyParagraph.add(deputyNameChunk);
+
+      subscribeParagraph.add(laboratoryParagraph);
+      subscribeParagraph.add(accountParagraph);
+      subscribeParagraph.add(deputyParagraph);
+
+      document.add(subscribeParagraph);
    }
 }
---
+++ baclab-reports/src/main/java/ua/com/hedgehogsoft/baclabreports/print/pdf/ReportPrinter.java
@@ -0,0 +1,58 @@
+package ua.com.hedgehogsoft.baclabreports.print.pdf;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.swing.JTable;
+import org.apache.log4j.Logger;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public abstract class ReportPrinter
+{
+   protected Font font;
+   protected String fileName;
+
+   public File print(JTable table, String... args)
+   {
+      File file = null;
+      try
+      {
+         BaseFont bf = null;
+         try
+         {
+            bf = BaseFont.createFont(System.getProperty("local.fonts"), BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+         }
+         catch (IOException e)
+         {
+            getLogger().error("The font wasn't loaded.", e);
+         }
+         font = new Font(bf);
+         font.setSize(14);
+         fileName = resolveFileName(args);
+         file = new File(fileName);
+         Document document = new Document(PageSize.A4, 0, 0, 0, 0);
+         PdfWriter.getInstance(document, new FileOutputStream(file));
+         document.open();
+         setContent(document, table, args);
+         document.close();
+      }
+      catch (Exception e)
+      {
+         getLogger().error(fileName + " wasn't printed.", e);
+      }
+      return file;
+   }
+
+   protected abstract Logger getLogger();
+
+   protected abstract String resolveFileName(String... args);
+
+   protected abstract void setContent(Document document, JTable table, String... args) throws DocumentException;
+}
